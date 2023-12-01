82e15ce14cb63d9b5ecced910e61a5f726509f8c
Vualerie <jumperz13577@gmail.com>
2023-12-01 15:44:16 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,21 @@
 using namespace std;
 
 int main(){
+    int num ;
+    int  even = 0, odd = 0 ;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> num;
+    while(num != 0){
+        if(num%2 == 0){
+                even += 1;
+            }
+        else{
+                odd += 1;
+            }
+        cout << "Enter an integer: ";
+        cin >> num;
+    }
+        cout << "#Even numbers = " << even << endl ;
+        cout << "#Odd numbers = " << odd ;
     return 0;
 }
